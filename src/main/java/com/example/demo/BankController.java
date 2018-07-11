b7edac025e4221f4b2c097b113846681b5fad1de
nitish24n <nitish24m@gmail.com>
2018-07-11 22:46:46 +0530

    Assessment 3
    health status of application

--- src/main/java/com/example/demo/BankController.java
+++ src/main/java/com/example/demo/BankController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,19 +9,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BankController {
-		
-	//Assignment 2 - Part 1
+
+	// Assignment 2 - Part 1
 	@RequestMapping("/branches/{bank}")
-    public String branches(@PathVariable(value="bank") String bank, Model model) {
-        model.addAttribute("name", bank);
-        return "branches";
-    }
-	
-	//Assignment 2 - Part 2
-		@RequestMapping("/services/{bank}")
-	    public String services(@PathVariable(value="bank") String bank, Model model) {
-	        model.addAttribute("name", bank);
-	        return "services";
-	    }
-	
+	public String branches(@PathVariable(value = "bank") String bank, Model model) {
+		model.addAttribute("name", bank);
+		return "branches";
+	}
+
+	// Assignment 2 - Part 2
+	@RequestMapping("/services/{bank}")
+	public String services(@PathVariable(value = "bank") String bank, Model model) {
+		model.addAttribute("name", bank);
+		return "services";
+	}
+
+	// Assignment 2 - Part 2
+	@RequestMapping("/health")
+	public String status(Model model) {
+		String status[] = {"UP","DOWN"};
+		Random random = new Random();
+		int index = random.nextInt(2);
+		model.addAttribute("status", status[index]);
+		return "status";
+	}
+
 }
